3e0f18d1bd62accb352bc0644a351c12c78f3764
Paulo Roberto <paulo.pereira.dev@gmail.com>
2025-08-13 20:56:07 -0300

    feature: alteracao 01

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,17 @@
- import express from 'express'
+import express from "express";
 
-import PointsController from './controllers/PointsController'
-const pointsController = new PointsController() 
+import PointsController from "./controllers/PointsController";
+const pointsController = new PointsController();
 
-import ItemController from './controllers/itemsController'
-const itemsController = new ItemController()
+import ItemController from "./controllers/itemsController";
+const itemsController = new ItemController();
 
-const routes = express()
+const routes = express();
 
-routes.get('/items', itemsController.index)
+routes.get("/items", itemsController.index);
 
-routes.post('/points', pointsController.create)
-routes.get('/points', pointsController.index)
-routes.get('/points/:id', pointsController.show)
+routes.post("/points1", pointsController.create);
+routes.get("/points", pointsController.index);
+routes.get("/points/:id", pointsController.show);
 
-export default routes
+export default routes;
